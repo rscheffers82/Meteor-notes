552539c7e804264031a153beb0164c48a5cadf8e
Roy Scheffers <r.scheffers@gmail.com>
2017-07-02 10:12:37 +0200

    add email address testcase

--- imports/api/users.test.js
+++ imports/api/users.test.js
@@ -1,12 +1,42 @@
+import { Meteor } from 'meteor/meteor';
 import expect from 'expect';
 
-const square = (a) => a * a;
+import { validateNewUser } from './users';
 
-describe('Square', ()=> {
-  it('should square a number', function() {
-    const res = square(5);
+if (Meteor.isServer) {
+  describe('users', () => {
 
-    expect(res).toBe(25);
-    }
+    it('should allow valid email address', () => {
+      const testUser = {
+        emails: [
+          { address: 'test@gmail.com' }
+        ]
+      }
+      const res = validateNewUser(testUser);
+      expect(res).toBe(true);
+    });
+
+    it('should reject invalid email', () => {
+      const testUser = {
+        emails: [
+          { address: '413fda756jgh' }
+        ]
+      }
+      expect(() => {
+        validateNewUser(testUser);
+      }).toThrow();
+    });
   });
-});
+};
+
+
+// const square = (a) => a * a;
+//
+// describe('Square', ()=> {
+//   it('should square a number', function() {
+//     const res = square(5);
+//
+//     expect(res).toBe(25);
+//     }
+//   });
+// });
