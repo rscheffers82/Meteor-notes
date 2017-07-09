d9f26f3102b099e17ccdf2e5f243526cffe94b57
Roy Scheffers <r.scheffers@gmail.com>
2017-07-09 15:17:24 +0200

    refactor mocha test suite to be more flexible

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -5,15 +5,17 @@ import { Notes } from './notes';
 if(Meteor.isServer) {
   describe('Notes', () => {
 
+    const noteOne = {
+      _id: 'testNoteId1',
+      title: 'My Title',
+      body: 'This is the body of the note',
+      updatedAt: 123,
+      userId: 'roy123'
+    };
+
     beforeEach(function () {
       Notes.remove({});
-      Notes.insert({
-        _id: 'testNoteId1',
-        title: 'My Title',
-        body: 'This is the body of the note',
-        updatedAt: 123,
-        userId: 'roy123'
-      });
+      Notes.insert(noteOne);
     });
 
     it('should insert new note', () => {
@@ -34,19 +36,19 @@ if(Meteor.isServer) {
     it('should remove note', () => {
       // apply (first argument sets the context, the this)
       // 2nd argument is an array with each value being the argument passed into the function that is called.
-      Meteor.server.method_handlers['notes.remove'].apply({ userId: 'roy123' }, ['testNoteId1']);
-      expect(Notes.findOne({ _id: 'testNoteId1' })).toNotExist();
+      Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId }, [noteOne._id]);
+      expect(Notes.findOne({ _id: noteOne._id })).toNotExist();
     });
 
     it('should not remove note if not authenticated', () => {
       expect(() => {
-        Meteor.server.method_handlers['notes.remove'].apply({}, ['testNoteId1']);
+        Meteor.server.method_handlers['notes.remove'].apply({}, [noteOne._id]);
       }).toThrow();
     });
 
-    it('should not remove note if invalid note _id', () => {
+    it('should not remove note if no invalid note _id', () => {
       expect(() => {
-        Meteor.server.method_handlers['notes.remove'].apply({ userId: 'roy123' });
+        Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId });
       }).toThrow();
     });
   });
