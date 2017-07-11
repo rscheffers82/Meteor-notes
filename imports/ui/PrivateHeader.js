1742937f08b83188f67f6dbe339a46a2d3194660
Roy Scheffers <r.scheffers@gmail.com>
2017-07-11 19:25:17 +0200

    add client test case of PrivateHeader logout functionality

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -6,14 +6,16 @@ const PrivateHeader = (props) => {
     <div className="header">
       <div className="header__content">
         <h1 className="header__title">{props.title}</h1>
-        <a className="button button--link-text" onClick={() => Accounts.logout()}>Logout</a>
+        <a className="button button--link-text" onClick={() => props.handleLogout()}>Logout</a>
+        {/* Accounts.logout() */}
       </div>
     </div>
   );
 };
 
 PrivateHeader.propTypes = {
-  title: React.PropTypes.string.isRequired
+  title: React.PropTypes.string.isRequired,
+  handleLogout: React.PropTypes.func.isRequired,
 };
 
 export default PrivateHeader;
--- imports/ui/PrivateHeader.test.js
+++ imports/ui/PrivateHeader.test.js
@@ -8,7 +8,7 @@ import PrivateHeader from './PrivateHeader';
 if (Meteor.isClient) {
   describe('PrivateHeader', () => {
     it('should set button text to logout', () => {
-      const wrapper = mount( <PrivateHeader title="Test title"/> )
+      const wrapper = mount( <PrivateHeader title="Test title" handleLogout={() =>{}} /> )
       const buttonText = wrapper.find('.button').text();
 
       expect(buttonText).toBe('Logout');
@@ -16,14 +16,29 @@ if (Meteor.isClient) {
 
     it('should use title prop as h1 text', () => {
       const title = 'Private header title';
-      const wrapper = mount( <PrivateHeader title={title} />);
+      const wrapper = mount( <PrivateHeader title={title} handleLogout={() =>{}} />);
       const headerTitle = wrapper.find('h1').text();
 
       expect(headerTitle).toBe(title);
     });
 
-    // it('', () => {
-    //
+    // it('should call the function (to test spies)', () => {
+    //   const spy = expect.createSpy();
+    //   spy(3,11);
+    //   // expect(spy).toHaveBeenCalled();
+    //   // expect(spy).toNotHaveBeenCalled();
+    //   expect(spy).toHaveBeenCalledWith(3, 11);
     // });
+
+    it('should call handleLogout on click', () => {
+      const spy = expect.createSpy();
+      const wrapper = mount( <PrivateHeader title="Test title" handleLogout={spy} /> );
+
+      wrapper.find('.button').simulate('click');
+
+      expect(spy).toHaveBeenCalled();
+    });
   });
 }
+
+git commit -am 'add client test case of PrivateHeader logout functionality'
