6423be399b2b3f8db3830a9ea03476e938d9476e
Roy Scheffers <r.scheffers@gmail.com>
2017-07-14 16:55:14 +0200

    create notesListItem component and update notesList

---
+++ imports/ui/NoteListItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import moment from 'moment';
+import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
+
+const NoteListItem = (props) => {
+  return (
+    <div>
+      <h5>{ props.note.title || 'Untitled note' }</h5>
+      <p>{ moment(props.note.updatedAt).format('M/DD/YYYY') }</p>
+    </div>
+  );
+};
+
+NoteListItem.propTypes = {
+  note: React.PropTypes.object.isRequired
+};
+
+export default NoteListItem;
