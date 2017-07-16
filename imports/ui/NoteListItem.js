f162f36b2dca3f503404d7231926222bee300fd0
Roy Scheffers <r.scheffers@gmail.com>
2017-07-16 09:45:26 +0200

    setup tracker for selected note

--- imports/ui/NoteListItem.js
+++ imports/ui/NoteListItem.js
@@ -2,10 +2,13 @@ import React from 'react';
 import moment from 'moment';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session';
 
-const NoteListItem = (props) => {
+export const NoteListItem = (props) => {
   return (
-    <div>
+    <div onClick={() => {
+      props.Session.set('selectedNoteId', props.note._id);
+    }}>
       <h5>{ props.note.title || 'Untitled note' }</h5>
       <p>{ moment(props.note.updatedAt).format('Do MMM YYYY') }</p>
     </div>
@@ -13,7 +16,10 @@ const NoteListItem = (props) => {
 };
 
 NoteListItem.propTypes = {
-  note: React.PropTypes.object.isRequired
+  note: React.PropTypes.object.isRequired,
+  Session: React.PropTypes.object.isRequired
 };
 
-export default NoteListItem;
+export default createContainer(() => {
+  return { Session }
+}, NoteListItem);
