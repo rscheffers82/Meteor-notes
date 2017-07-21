1f276ced48914b33dc20a748e8823fab01b8f13d
Roy Scheffers <r.scheffers@gmail.com>
2017-07-21 16:49:25 +0200

    initial setup for mobile ready menu

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Accounts  } from 'meteor/accounts-base';
 import { createContainer } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session';
 
 export const PrivateHeader = (props) => {
+  const navImageSrc = props.isNavOpen ? '/images/x.svg' : '/images/bars.svg';
+
   return (
     <div className="header">
       <div className="header__content">
+        <img src={navImageSrc} onClick={props.toggleNav}/>
         <h1 className="header__title">{props.title}</h1>
         <a className="button button--link-text" onClick={() => props.handleLogout()}>Logout</a>
         {/* Accounts.logout() */}
@@ -17,10 +21,14 @@ export const PrivateHeader = (props) => {
 PrivateHeader.propTypes = {
   title: React.PropTypes.string.isRequired,
   handleLogout: React.PropTypes.func.isRequired,
+  toggleNav: React.PropTypes.func.isRequired,
+  isNavOpen: React.PropTypes.bool.isRequired,
 };
 
 export default createContainer(() => {
   return {
-    handleLogout: () => Accounts.logout()
+    handleLogout: () => Accounts.logout(),
+    toggleNav: () => Session.set('isNavOpen', !Session.get('isNavOpen')),
+    isNavOpen: Session.get('isNavOpen'),
   }
 }, PrivateHeader);
